bfa7e06eaab0726f89ecef5b4b33474ecddaecd8
Marina <marinabrito.ma@gmail.com>
2025-03-02 18:37:53 +0000

    added swagger documentation

--- src/main/java/com/javanauta/transaction_api/controller/StatisticController.java
+++ src/main/java/com/javanauta/transaction_api/controller/StatisticController.java
@@ -2,8 +2,12 @@ package com.javanauta.transaction_api.controller;
 
 import com.javanauta.transaction_api.business.services.StatisticService;
 import com.javanauta.transaction_api.controller.dtos.StatisticResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,13 @@ public class StatisticController {
 
     private final StatisticService statisticService;
 
+    @GetMapping
+    @Operation(description = "Endpoint responsavel por buscar transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro nabusca de estatiticas de transactions"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<StatisticResponseDTO> findStatistics(
             @RequestParam(value = "intervalSearch", required = false, defaultValue = "60")
                                                                Integer intervalSearch){
--- src/main/java/com/javanauta/transaction_api/controller/TransactionController.java
+++ src/main/java/com/javanauta/transaction_api/controller/TransactionController.java
@@ -2,6 +2,9 @@ package com.javanauta.transaction_api.controller;
 
 import com.javanauta.transaction_api.business.services.TransactionService;
 import com.javanauta.transaction_api.controller.dtos.TransactionRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,13 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping
+    @Operation(description = "Endpoint responsavel por adicionar transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos nao atendem os requisitos da transaction"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisicao"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> addTransaction(
             @RequestBody TransactionRequestDTO dto){
         transactionService.addTransactions(dto);
@@ -22,6 +32,12 @@ public class TransactionController {
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsavel por deletar transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction deletada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisicao"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> deleteTransaction(){
         transactionService.clearTransactions();
         return ResponseEntity.ok().build();
